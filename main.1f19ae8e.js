1034809f5756d9530e7bdd8a29138fc42fc6ce99
dodortus <dodortus@gmail.com>
2020-11-13 01:06:20 +0900

    Updates

--- main.1f19ae8e.js
+++ main.1f19ae8e.js
@@ -541,7 +541,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8309" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8538" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
